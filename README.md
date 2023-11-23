00fa63b5e3cbac2b668fbe88734e4de8c3331755
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2023-11-22 20:45:06 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Stuy Bioinformatics Site
+
+## How to run
+
+Use npm:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:8080](http://localhost:8080) with your browser to see the result.
---
+++ _data/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+	title: "Stuy Bioinformatics",
+	url: "https://stuybioinformatics.org/",
+	language: "en",
+	description: "Stuy Bioinformatics creates a space where students express their interests, work on projects, and publish articles on the applications of computer science in biology.",
+};
